81dd30bf1b8f4579b3e10f5fdb96338d916ab3ac
Matt Fishman <mtfishman@users.noreply.github.com>
2025-03-07 16:49:20 -0500

    Update constructors (#17)

--- src/diagonalarray/diagonalarray.jl
+++ src/diagonalarray/diagonalarray.jl
@@ -2,90 +2,103 @@ function getzero(a::AbstractArray{<:Any,N}, I::Vararg{Int,N}) where {N}
   return zero(eltype(a))
 end
 
+function _DiagonalArray end
+
 struct DiagonalArray{T,N,Diag<:AbstractVector{T},F} <: AbstractDiagonalArray{T,N}
   diag::Diag
-  dims::NTuple{N,Int}
-  getunstoredindex::F
+  dims::Dims{N}
+  getunstored::F
+  global @inline function _DiagonalArray(
+    diag::Diag, dims::Dims{N}, getunstored::F
+  ) where {T,N,Diag<:AbstractVector{T},F}
+    all(≥(0), dims) || throw(ArgumentError("Invalid dimensions: $dims"))
+    length(diag) == minimum(dims) ||
+      throw(ArgumentError("Length of diagonals doesn't match dimensions"))
+    return new{T,N,Diag,F}(diag, dims, getunstored)
+  end
 end
 
 function DiagonalArray{T,N}(
-  diag::AbstractVector{T}, d::Tuple{Vararg{Int,N}}, getunstoredindex=getzero
+  diag::AbstractVector, dims::Dims{N}; getunstored=getzero
 ) where {T,N}
-  return DiagonalArray{T,N,typeof(diag),typeof(getunstoredindex)}(diag, d, getunstoredindex)
+  return _DiagonalArray(convert(AbstractVector{T}, diag), dims, getunstored)
 end
 
 function DiagonalArray{T,N}(
-  diag::AbstractVector, d::Tuple{Vararg{Int,N}}, getunstoredindex=getzero
+  diag::AbstractVector, dims::Vararg{Int,N}; kwargs...
 ) where {T,N}
-  return DiagonalArray{T,N}(T.(diag), d, getunstoredindex)
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
 end
 
-function DiagonalArray{T,N}(diag::AbstractVector, d::Vararg{Int,N}) where {T,N}
-  return DiagonalArray{T,N}(diag, d)
+function DiagonalArray{T}(diag::AbstractVector, dims::Dims{N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
 end
 
-function DiagonalArray{T}(
-  diag::AbstractVector, d::Tuple{Vararg{Int,N}}, getunstoredindex=getzero
+function DiagonalArray{T}(diag::AbstractVector, dims::Vararg{Int,N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
+end
+
+function DiagonalArray{<:Any,N}(
+  diag::AbstractVector{T}, dims::Dims{N}; kwargs...
 ) where {T,N}
-  return DiagonalArray{T,N}(diag, d, getunstoredindex)
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
 end
 
-function DiagonalArray{T}(diag::AbstractVector, d::Vararg{Int,N}) where {T,N}
-  return DiagonalArray{T,N}(diag, d)
+function DiagonalArray{<:Any,N}(
+  diag::AbstractVector{T}, dims::Vararg{Int,N}; kwargs...
+) where {T,N}
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
 end
 
-function DiagonalArray(diag::AbstractVector{T}, d::Tuple{Vararg{Int,N}}) where {T,N}
-  return DiagonalArray{T,N}(diag, d)
+function DiagonalArray(diag::AbstractVector{T}, dims::Dims{N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
 end
 
-function DiagonalArray(diag::AbstractVector{T}, d::Vararg{Int,N}) where {T,N}
-  return DiagonalArray{T,N}(diag, d)
+function DiagonalArray(diag::AbstractVector{T}, dims::Vararg{Int,N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(diag, dims; kwargs...)
 end
 
 # Infer size from diagonal
-function DiagonalArray{T,N}(diag::AbstractVector) where {T,N}
-  return DiagonalArray{T,N}(diag, ntuple(Returns(length(diag)), N))
+function DiagonalArray{T,N}(diag::AbstractVector; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(diag, ntuple(Returns(length(diag)), N); kwargs...)
 end
 
-function DiagonalArray{<:Any,N}(diag::AbstractVector{T}) where {T,N}
-  return DiagonalArray{T,N}(diag)
+function DiagonalArray{<:Any,N}(diag::AbstractVector{T}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(diag; kwargs...)
 end
 
 # undef
-function DiagonalArray{T,N}(
-  ::UndefInitializer, d::Tuple{Vararg{Int,N}}, getunstoredindex=getzero
-) where {T,N}
-  return DiagonalArray{T,N}(Vector{T}(undef, minimum(d)), d, getunstoredindex)
+function DiagonalArray{T,N}(::UndefInitializer, dims::Dims{N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(Vector{T}(undef, minimum(dims)), dims; kwargs...)
 end
 
-function DiagonalArray{T,N}(::UndefInitializer, d::Vararg{Int,N}) where {T,N}
-  return DiagonalArray{T,N}(undef, d)
+function DiagonalArray{T,N}(::UndefInitializer, dims::Vararg{Int,N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(undef, dims; kwargs...)
 end
 
-function DiagonalArray{T}(
-  ::UndefInitializer, d::Tuple{Vararg{Int,N}}, getunstoredindex=getzero
-) where {T,N}
-  return DiagonalArray{T,N}(undef, d, getunstoredindex)
+function DiagonalArray{T}(::UndefInitializer, dims::Dims{N}; kwargs...) where {T,N}
+  return DiagonalArray{T,N}(undef, dims; kwargs...)
+end
+
+function DiagonalArray{T}(::UndefInitializer, dims::Vararg{Int,N}) where {T,N}
+  return DiagonalArray{T,N}(undef, dims)
 end
 
 # Axes version
 function DiagonalArray{T}(
-  ::UndefInitializer, axes::Tuple{Vararg{AbstractUnitRange,N}}, getunstoredindex=getzero
+  ::UndefInitializer, axes::NTuple{N,Base.OneTo{Int}}; kwargs...
 ) where {T,N}
-  @assert all(isone, first.(axes))
-  return DiagonalArray{T,N}(undef, length.(axes), getunstoredindex)
-end
-
-function DiagonalArray{T}(::UndefInitializer, d::Vararg{Int,N}) where {T,N}
-  return DiagonalArray{T,N}(undef, d)
+  return DiagonalArray{T,N}(undef, length.(axes); kwargs...)
 end
 
 # Minimal `AbstractArray` interface
 Base.size(a::DiagonalArray) = a.dims
 
 function Base.similar(a::DiagonalArray, elt::Type, dims::Tuple{Vararg{Int}})
-  # TODO: Preserve zero element function.
-  return DiagonalArray{elt}(undef, dims, a.getunstoredindex)
+  function getzero(a::AbstractArray{<:Any,N}, I::Vararg{Int,N}) where {N}
+    return convert(elt, a.getunstored(a, I...))
+  end
+  return DiagonalArray{elt}(undef, dims; getunstored=getzero)
 end
 
 # DiagonalArrays interface.
