2d73ebad0fcbc7937aa10c9f65d8b2208a089bd0
mtfishman <mfishman@flatironinstitute.org>
2025-07-29 16:54:14 -0400

    Format

--- test/test_tensoralgebraext.jl
+++ test/test_tensoralgebraext.jl
@@ -7,5 +7,7 @@ using Test: @test, @testset
   a = Delta{Float32}(2, 2, 2)
   m = matricize(a, (1,), (2, 3))
   @test m ≡ Eye{Float32}(2, 4)
-  @test unmatricize(FusionStyle(a), m, tuplemortar(((axes(a, 1),), (axes(a, 2), axes(a, 3))))) ≡ a
+  @test unmatricize(
+    FusionStyle(a), m, tuplemortar(((axes(a, 1),), (axes(a, 2), axes(a, 3))))
+  ) ≡ a
 end
